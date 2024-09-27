9565c00a8a3055fda5c681dc736aba3b10dfae18
Christopher Patton <cpatton@cloudflare.com>
2024-09-27 08:26:18 -0700

    Add reset script

---
+++ interop-divviup/reset.sh
@@ -0,0 +1,11 @@
+# Clear the storage proxy
+curl -X POST http://localhost:8787/internal/delete_all
+curl -X POST http://localhost:8788/internal/delete_all
+
+# Generate HPKE config
+cargo run --bin dapf -- test-routes add-hpke-config http://localhost:8787/v09/ --kem-alg x25519_hkdf_sha256
+cargo run --bin dapf -- test-routes add-hpke-config http://localhost:8788/v09/ --kem-alg x25519_hkdf_sha256
+
+# Configure task
+curl -X POST 'http://localhost:8787/v09/internal/test/add_task' -H 'Content-Type: application/json' -d @leader_task.json
+curl -X POST 'http://localhost:8788/v09/internal/test/add_task' -H 'Content-Type: application/json' -d @helper_task.json
